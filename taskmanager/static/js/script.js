f1c48bf081179fc6b4a0c8256748f24eb049d02c
merzann <annisch78@gmail.com>
2024-12-10 00:38:25 +0000

    feat: add modal to categories.html, wire up modal to Delete-Button to warn user that action is irreversible, add CSS to style modal

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    M.Modal.init(elems);
+});
